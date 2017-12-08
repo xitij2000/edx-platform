2aee2a7d4d569a9b177e122dfd589108630a6634
Matt Tuchfarber <mtuchfarber@edx.org>
2017-12-08 16:20:31 -0500

    Remove old JS

--- lms/static/js/program_marketing.js
+++ lms/static/js/program_marketing.js
@@ -4,17 +4,3 @@ function playVideo(src) {
     document.querySelector('#program_video iframe').style = 'display:block;';
     document.querySelector('#program_video iframe').src = src;
 }
-function toggleBio(currentEl, bioHash) {
-    'use strict';
-    var button = currentEl;
-    var bioId = 'instructor-bio-' + bioHash;
-    var bio = document.getElementById(bioId);
-    var hidden = bio.classList.contains('hidden');
-    if (hidden) {
-        bio.classList.remove('hidden');
-        button.innerHTML = 'Hide Bio';
-    } else {
-        bio.classList.add('hidden');
-        button.innerHTML = 'View Bio';
-    }
-}
